c7482dfe130bc727e7d74ad0861d14b8cc1d880a
daormar <daniel.ortiz.phd@gmail.com>
2024-06-23 23:25:41 +0200

    Add csv file sorting code.

--- plot_n_expers.sh
+++ plot_n_expers.sh
@@ -20,7 +20,10 @@ tmpfile=`mktemp`
 find "${results_basedir}" -name "time.out" -exec awk '{printf"%s\n",$0}' {} \; | grep "${expertype} ${num_procs}" > "${tmpfile}"
 
 # Generate csv file
-python "${pkgdir}/utils/gen_n_expers_data.py" "${tmpfile}" > "${resultsdir}/n_expers_data.csv"
+python "${pkgdir}/utils/gen_n_expers_data.py" "${tmpfile}" > "${resultsdir}/n_expers_data_unsorted.csv"
+
+# Sort csv file
+(head -n 1 "${resultsdir}/n_expers_data_unsorted.csv" && tail -n +2 "${resultsdir}/n_expers_data_unsorted.csv" | sort -t, -k1,1n) > "${resultsdir}/n_expers_data.csv"
 
 # Generate plot
 cp "${pkgdir}/utils/n_expers.gp" "${resultsdir}"
