99f1a222a2692f2e6fcfaaa2b53f02408d2e11cf
daormar <daniel.ortiz.phd@gmail.com>
2024-06-28 15:13:22 +0200

    Fix treatment of more than 2 data sources.

--- plot_cpu_evol.sh
+++ plot_cpu_evol.sh
@@ -22,18 +22,21 @@ find "${results_basedir}" -name "*record.txt" -exec cp {} "${resultsdir}" \;
 cp "${pkgdir}/utils/cpu_evol_template.gp" "${resultsdir}/cpu_evol.gp"
 pushd "${resultsdir}"
 i=1
+plot_command="plot "
 for file in "${resultsdir}"/*record.txt; do
     basefile="$(basename $file)"
     basefile_corrected="$(basename $file).corrected"
     awk '{if($2 < 0.0001) $2=0.0001; print $0}' "$file" > "${basefile_corrected}"
     if [ "$i" -eq 1 ]; then
-        echo plot \"${basefile_corrected}\" using 1:2 with linespoints linestyle "${i}" title \'${basefile}\'\, "\\" >> "${resultsdir}/cpu_evol.gp"
+        sep=""
     else
-        echo \"${basefile_corrected}\" using 1:2 with linespoints linestyle "${i}" title \'${basefile}\' >> "${resultsdir}/cpu_evol.gp"
+        sep=","
     fi
-
+    plot_command="${plot_command} ${sep} \"${basefile_corrected}\" using 1:2 with lines linestyle ${i} title '${basefile}'"
     i=$((i + 1))
 done
+
+echo "${plot_command}" >> "${resultsdir}/cpu_evol.gp"
 
 gnuplot "cpu_evol.gp"
 popd
--- plot_mem_evol.sh
+++ plot_mem_evol.sh
@@ -22,18 +22,21 @@ find "${results_basedir}" -name "*record.txt" -exec cp {} "${resultsdir}" \;
 cp "${pkgdir}/utils/mem_evol_template.gp" "${resultsdir}/mem_evol.gp"
 pushd "${resultsdir}"
 i=1
+plot_command="plot "
 for file in "${resultsdir}"/*record.txt; do
     basefile="$(basename $file)"
     basefile_corrected="$(basename $file).corrected"
     awk '{if($2 < 0.0001) $2=0.0001; print $0}' "$file" > "${basefile_corrected}"
     if [ "$i" -eq 1 ]; then
-        echo plot \"${basefile_corrected}\" using 1:3 with linespoints linestyle "${i}" title \'${basefile}\'\, "\\" >> "${resultsdir}/mem_evol.gp"
+        sep=""
     else
-        echo \"${basefile_corrected}\" using 1:3 with linespoints linestyle "${i}" title \'${basefile}\' >> "${resultsdir}/mem_evol.gp"
+        sep=","
     fi
-
+    plot_command="${plot_command} ${sep} \"${basefile_corrected}\" using 1:3 with lines linestyle ${i} title '${basefile}'"
     i=$((i + 1))
 done
+
+echo "${plot_command}" >> "${resultsdir}/mem_evol.gp"
 
 gnuplot "mem_evol.gp"
 popd
